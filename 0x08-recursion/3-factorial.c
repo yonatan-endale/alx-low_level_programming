a3db47eed5aec7f614567347af088fe3a7aa6051
yonatan-endale <88444847+yonatan-endale@users.noreply.github.com>
2022-09-28 19:36:12 +0300

    recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *factorial - check the code.
+ *@n: character
+ * Return: Always 0.
+ */
+int factorial(int n)
+{
+int fact = 0;
+if (n < 0)
+{
+return (-1);
+}
+if (n == 0)
+{
+return (0);
+}
+if (n == 1)
+{
+return (1);
+}
+fact = n * factorial(n - 1);
+return (fact);
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * prime - checks number is prime
+ * @a:  check
+ * @j:  prime number
+ *
+ * Return: 1 if prime, 0 if not
+ */
+int prime(int a, int j)
+{
+if (j < 2 || j % a == 0)
+return (0);
+else if (a > j / 2)
+return (1);
+else
+return (prime(a + 1, j));
+}
+/**
+ * is_prime_number - states if number is prime
+ * @n: number to check
+ *
+ * Return: 1 if prime, 0 if not
+ */
+int is_prime_number(int n)
+{
+if (n == 2)
+return (1);
+return (prime(2, n));
+}
